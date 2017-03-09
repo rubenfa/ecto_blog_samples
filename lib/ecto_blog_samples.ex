e46a0957dd83cd61f65989d11adec4317b58d3ae
rubenfa <ruben.fernandez.alfonso@gmail.com>
2017-03-09 14:47:41 +0100

    init

---
+++ lib/ecto_blog_samples.ex
@@ -0,0 +1,18 @@
+defmodule EctoBlogSamples do
+  @moduledoc """
+  Documentation for EctoBlogSamples.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoBlogSamples.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_blog_samples_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoBlogSamplesTest do
+  use ExUnit.Case
+  doctest EctoBlogSamples
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
